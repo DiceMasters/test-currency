feb3c7cbc1cd295b42a6ecce47a21ac1e35e71b7
Albert Saitov <a.saitov@dcrt.it>
2022-01-25 08:23:31 +0500

    Feat: Add errors handler, setup components

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
     <v-main>
       <router-view/>
     </v-main>
+
+    <v-snackbar v-model="snackbar"
+                vertical>
+      {{ errorText }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn text
+               v-bind="attrs"
+               @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -12,11 +25,27 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 import CHeader from '@/components/layout/Header'
+import bus from './utils/bus'
 
 @Component({
   components: {
     'c-header': CHeader
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  errorText = ''
+  snackbar = false
+
+  created (): void {
+    bus.$on('updateError', (errorText: string) => {
+      this.errorText = errorText
+      this.snackbar = true
+    })
+  }
+
+  resetError (): void {
+    this.errorText = ''
+    this.snackbar = false
+  }
+}
 </script>
