9e3d34477de825447aa677d718cf549e3144f886
Albert Saitov <a.saitov@dcrt.it>
2022-01-25 08:24:02 +0500

    Refactor: Remove select block

--- src/components/layout/Header/Header.vue
+++ src/components/layout/Header/Header.vue
@@ -3,7 +3,6 @@
              class="c-header"
              elevation="0"
              color="green"
-             prominent
              dark>
     <v-container>
       <v-row>
@@ -15,30 +14,20 @@
 
             <v-spacer/>
 
-            <v-btn text
+            <v-btn class="mr-2"
+                   text
                    :to="{name: 'Конвертер'}">
-              <span class="mr-2">
+              <span>
                 Конвертер
               </span>
             </v-btn>
 
             <v-btn text
                    :to="{name: 'Курсы валют'}">
-              <span class="mr-2">
+              <span>
                 Курсы валют
               </span>
             </v-btn>
-
-            <v-spacer/>
-
-            <v-select v-model="baseCurrency"
-                      :items="currencies"
-                      chips
-                      attach
-                      outlined
-                      small-chips
-                      label="Base Currency"
-                      @change="updateBaseCurrency"/>
           </div>
         </v-col>
       </v-row>
@@ -49,22 +38,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
-import { TCurrency } from '@/types/main'
-import bus from '@/utils/bus'
-
 @Component
-export default class CHeader extends Vue {
-  baseCurrency: TCurrency = 'USD'
-  currencies: TCurrency[] = [
-    'RUB',
-    'USD',
-    'EUR'
-  ]
-
-  updateBaseCurrency (value: TCurrency): void {
-    bus.$emit('updateBaseCurrency', value)
-  }
-}
+export default class CHeader extends Vue {}
 </script>
 
 <style lang="scss">
