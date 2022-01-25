e974a5235d93ae8d3b5c494d5574240c010cd8cd
Albert Saitov <a.saitov@dcrt.it>
2022-01-25 08:51:27 +0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-process.env.API_KEY = 'a0c1aa70-7d13-11ec-adf5-13fde3e2676c'
-
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '',
   transpileDependencies: [
     'vuetify'
   ]
